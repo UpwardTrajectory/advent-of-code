9d1c04d21371ec2f7b0b6b7c460772a6eb16586e
David Kaspar <44875366+UpwardTrajectory@users.noreply.github.com>
2021-12-04 11:11:15 -0500

    fixed some type hinting, then got inspired to refactor Bingo() so it only needs to instantiate & loop once, rather than needing a hard reset between P1 & P2

--- 2021/day04/puzzle.py
+++ 2021/day04/puzzle.py
@@ -1,6 +1,5 @@
-import pandas as pd
 import numpy as np
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 
 @dataclass
@@ -8,58 +7,59 @@ class Board:
     raw: str
     state: np.array = None
 
-    def __post_init__(self):
+    def __post_init__(self) -> None:
         if self.state is None:
             self.state = np.zeros((5, 5), dtype=bool)
 
-    def __repr__(self):
-        return f"Board({self.orig[0]}...)"
+    def __repr__(self) -> str:
+        return f"Board({self.number_grid[0]}...)"
 
     @property
-    def orig(self):
+    def number_grid(self) -> np.array:
         board = [
             row.strip().replace("  ", " ").split(" ") for row in self.raw.split("\n")
         ]
         return np.array(board).astype(int)
 
     @property
-    def winner(self):
+    def winner(self) -> bool:
         horiz = self.state.all(axis=1).any()
         vert = self.state.all(axis=0).any()
         return horiz | vert
 
     def mark_num(self, num: int):
-        self.state[np.where(self.orig == num)] = True
+        self.state[np.where(self.number_grid == num)] = True
 
-    def final_score(self, winning_draw):
-        unmarked_sum = (self.orig * (1 - self.state)).sum()
+    def final_score(self, winning_draw: int) -> int:
+        unmarked_sum = (self.number_grid * (1 - self.state)).sum()
         return winning_draw * unmarked_sum
 
 
 @dataclass
 class Bingo:
+    """refactored after knowing part 2 needed all games to eventually win"""
     draws: np.array
     boards: list[Board]
-
-    def play(self):
+    winning_score: int = None
+    last_loser_score: int = None
+    win_order: list[Board] = field(default_factory=list)
+        
+    def play_all(self) -> None:
         for draw in self.draws:
             for board in self.boards:
                 board.mark_num(draw)
                 if board.winner:
-                    return board.final_score(draw)
-
-    def last_board(self):
-        win_order = []
-        for draw in self.draws:
-            for board in self.boards:
-                board.mark_num(draw)
-                if board.winner and board not in win_order:
-                    win_order.append(board)
-                if len(win_order) == len(self.boards):
-                    return board.final_score(draw)
+                    winning_score = board.final_score(draw)
+                    if self.winning_score is None:
+                        self.winning_score = winning_score
+                    if board not in self.win_order:
+                        self.win_order.append(board)
+                    if len(self.win_order) == len(self.boards):
+                        self.last_loser_score = winning_score
+                        return
 
 
-def parse_raw(fname: str) -> tuple[np.array, list[np.array]]:
+def parse_raw(fname: str) -> tuple[np.array, list[Board]]:
     with open(fname, "r") as f:
         raw = f.read().strip()
 
@@ -69,24 +69,12 @@ def parse_raw(fname: str) -> tuple[np.array, list[np.array]]:
     return draws, boards
 
 
-def play_game(fname: str) -> int:
-    draws, boards = parse_raw(fname)
-    game = Bingo(draws, boards)
-    return game.play()
-
-
-def find_last_board_score(fname: str) -> int:
-    draws, boards = parse_raw(fname)
-    game = Bingo(draws, boards)
-    return game.last_board()
-
-
-draws, boards = parse_raw("test.txt")
-game = Bingo(draws, boards)
-t1 = game.play()
+game = Bingo(*parse_raw("test.txt"))
+game.play_all()
+t1 = game.winning_score
 assert t1 == 4512, f"Test 1 failed. Got {t1} instead of 4512."
 
-t2 = game.last_board()
+t2 = game.last_loser_score
 assert t2 == 1924, f"Test 2 failed. Got {t2} instead of 1924."
 
 
@@ -94,8 +82,11 @@ print("All tests passed.")
 
 
 if __name__ == "__main__":
-    p1 = play_game("inputs.txt")
+    game = Bingo(*parse_raw("inputs.txt"))
+    game.play_all()
+    
+    p1 = game.winning_score
     print("Part 1:", p1)
 
-    p2 = find_last_board_score("inputs.txt")
+    p2 = game.last_loser_score
     print("Part 2:", p2)
