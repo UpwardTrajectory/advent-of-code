6cd8bf8caa882dd21a0d7ca4361ee8c17c491ff9
David Kaspar <44875366+UpwardTrajectory@users.noreply.github.com>
2021-12-13 18:53:12 -0500

    complete day 12 (after looking @ https://github.com/joelgrus/advent2021/blob/master/day12.py)

--- 2021/day12/puzzle.py
+++ 2021/day12/puzzle.py
@@ -1,104 +1,87 @@
+"""I ... caved ... and looked at an answer:
+
+final working solution inspired by:
+https://github.com/joelgrus/advent2021/blob/master/day12.py
+"""
+
 from pathlib import Path
+from collections import defaultdict
 from dataclasses import dataclass, field
-from statistics import median
 
 
 T1_ANS = 10
-T2_ANS = None
+T2_ANS = 36
 
-
-@dataclass
-class Cave:
-    name: str
-    revisit: bool
-    seen: bool = False
-    neighbors: set = field(default_factory=set, repr=False)   
-    
-    def __hash__(self):
-        return hash(self.name)
-    
-    def check_all_visited(self) -> bool:
-        all_visited = False
-        for c in self.neighbors:
-            if not c.seen:
-                continue
-            if not c.revisit:
-                
-        if all_seen:
-            if self.name == "A":
-                print(self.neighbors)
-                print("closing A")
-            self.revisit = False
-#             print("<<closing off", self.name, end=">>")
-            return True
-        return False
-    
 
 @dataclass
 class Puzzle:
     fname: str
-    caves: dict[str: Cave] = field(default_factory=dict, repr=False)
-#     visited: set[Cave] = field(default_factory=set, repr=False)
-    paths: int = 0
-    all_path_routes: list[str] = field(default_factory=list, repr=False)
+    caves: dict[str:list] = field(default_factory=lambda: defaultdict(list), repr=False)
+    all_path_routes: set[str] = field(default_factory=set, repr=False)
 
     def __post_init__(self):
-        raw = Path(self.fname).open().readlines()     
+        raw = Path(self.fname).open().readlines()
         for conn in raw:
-            a, b = conn.strip().split("-")         
-            for cave_name in (a, b):
-                if cave_name not in self.caves:
-                    if cave_name == cave_name.lower():
-                        self.caves[cave_name] = Cave(cave_name, False)
-                    else:
-                        self.caves[cave_name] = Cave(cave_name, True)            
-            self.connect(self.caves[a], self.caves[b])
+            a, b = conn.strip().split("-")
+            self.caves[a].append(b)
+            self.caves[b].append(a)
         return
-    
-    def connect(self, a, b):
-        a.neighbors.add(b)
-        b.neighbors.add(a)
-        return        
-        
-    def dfs(self, cave=None, cur_path=None):
-        if cave is None:
-            cave = self.caves['start']
-            cur_path = "start"
-        
-        cave.seen = True
-    
-        if cave.name == 'end':
-            self.paths += 1
-            self.all_path_routes.append(cur_path)
-            if cave.check_all_visited():
-                print("------- All paths explored ------")
+
+    @property
+    def num_paths(self):
+        return len(self.all_path_routes)
+
+    def find_valid_paths(self):
+
+        unexplored = [["start"]]
+
+        while unexplored:
+            cur_path = unexplored.pop()
+            cave = cur_path[-1]
+
+            if cave == "end":
+                self.all_path_routes.add(",".join(cur_path))
             else:
-                return self.dfs()
-            
-        cave.check_all_visited()
-        
-        for neighbor in cave.neighbors:                
-            if neighbor.revisit or not neighbor.seen:
-                updated_path = cur_path + "," + neighbor.name
-                self.dfs(neighbor, cur_path=updated_path)     
-                
-        return  
-    
-    def find_many(self):
-        n = 0
-        while True:
-            if n > 1000:
-                break
-            self.dfs()
-            n += 1
-        return self.paths
-            
+                for next_cave in self.caves[cave]:
+                    if next_cave not in cur_path or next_cave.isupper():
+                        unexplored.append(cur_path + [next_cave])
+        return
+
+    def find_double_small_paths(self):
+        small_caves = {
+            cave
+            for cave in self.caves
+            if cave.islower() and cave not in ("start", "end")
+        }
+        unexplored = [["start"]]
+
+        while unexplored:
+            cur_path = unexplored.pop()
+            cave = cur_path[-1]
+
+            if cave == "end":
+                self.all_path_routes.add(",".join(cur_path))
+            else:
+                for next_cave in self.caves[cave]:
+                    double_smalls = [
+                        cave for cave in small_caves if cur_path.count(cave) > 1
+                    ]
+                    should_explore = (
+                        next_cave.isupper()
+                        or (not double_smalls and next_cave != "start")
+                        or (next_cave not in cur_path)
+                    )
+                    if should_explore:
+                        unexplored.append(cur_path + [next_cave])
+        return
 
     def part_1(self):
-        pass
+        self.find_valid_paths()
+        return self.num_paths
 
     def part_2(self):
-        pass
+        self.find_double_small_paths()
+        return self.num_paths
 
 
 def run_tests(p1_ans=T1_ANS, p2_ans=T2_ANS, fname="tests.txt"):
@@ -116,9 +99,8 @@ def run_tests(p1_ans=T1_ANS, p2_ans=T2_ANS, fname="tests.txt"):
 
 if __name__ == "__main__":
     run_tests()
-    run_tests(19, None, fname='tests2.txt')
-    run_tests(226, None, fname='tests3.txt')
-
+    run_tests(19, 103, fname="tests2.txt")
+    run_tests(226, 3509, fname="tests3.txt")
 
     puz = Puzzle("inputs.txt")
 
