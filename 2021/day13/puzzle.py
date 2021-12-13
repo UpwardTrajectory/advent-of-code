ce3b84af09d1b61313e4d686082ef36cf6b2d051
David Kaspar <44875366+UpwardTrajectory@users.noreply.github.com>
2021-12-13 16:40:37 -0500

    begin work on day 13

--- 2021/day13/puzzle.py
+++ 2021/day13/puzzle.py
@@ -1,103 +1,95 @@
 from pathlib import Path
+from typing import Literal
 from dataclasses import dataclass, field
+import numpy as np
 
 
-T1_ANS = 10
+T1_ANS = 17
 T2_ANS = None
 
 
 @dataclass
-class Cave:
-    name: str
-    revisit: bool
-    seen: bool = False
-    neighbors: set = field(default_factory=set, repr=False)   
-    
-    def __hash__(self):
-        return hash(self.name)
-    
-    def check_all_visited(self) -> bool:
-        all_visited = False
-        for c in self.neighbors:
-            if not c.seen:
-                continue
-            if not c.revisit:
-                
-        if all_seen:
-            if self.name == "A":
-                print(self.neighbors)
-                print("closing A")
-            self.revisit = False
-#             print("<<closing off", self.name, end=">>")
-            return True
-        return False
-    
-
-@dataclass
 class Puzzle:
     fname: str
-    caves: dict[str: Cave] = field(default_factory=dict, repr=False)
-#     visited: set[Cave] = field(default_factory=set, repr=False)
-    paths: int = 0
-    all_path_routes: list[str] = field(default_factory=list, repr=False)
+    raw_coords: str = None
+    grid: np.array = None
+    folds: list[tuple[str, int]] = field(default_factory=list, repr=False)
 
     def __post_init__(self):
-        raw = Path(self.fname).open().readlines()     
-        for conn in raw:
-            a, b = conn.strip().split("-")         
-            for cave_name in (a, b):
-                if cave_name not in self.caves:
-                    if cave_name == cave_name.lower():
-                        self.caves[cave_name] = Cave(cave_name, False)
-                    else:
-                        self.caves[cave_name] = Cave(cave_name, True)            
-            self.connect(self.caves[a], self.caves[b])
+        raw = Path(self.fname).open().read()
+        coords, folds = raw.split("\n\n")
+        self.raw_coords = coords
+        self.grid = self.parse_coords(coords)
+
+        folds = [x[11:].split("=") for x in folds.split("\n")]
+        self.folds = [(across_dim, int(val)) for across_dim, val in folds]
         return
-    
-    def connect(self, a, b):
-        a.neighbors.add(b)
-        b.neighbors.add(a)
-        return        
-        
-    def dfs(self, cave=None, cur_path=None):
-        if cave is None:
-            cave = self.caves['start']
-            cur_path = "start"
-        
-        cave.seen = True
-    
-        if cave.name == 'end':
-            self.paths += 1
-            self.all_path_routes.append(cur_path)
-            if cave.check_all_visited():
-                print("------- All paths explored ------")
-            else:
-                return self.dfs()
-            
-        cave.check_all_visited()
-        
-        for neighbor in cave.neighbors:                
-            if neighbor.revisit or not neighbor.seen:
-                updated_path = cur_path + "," + neighbor.name
-                self.dfs(neighbor, cur_path=updated_path)     
-                
-        return  
-    
-    def find_many(self):
-        n = 0
-        while True:
-            if n > 1000:
-                break
-            self.dfs()
-            n += 1
-        return self.paths
-            
+
+    @staticmethod
+    def parse_coords(coords: str) -> np.array:
+        """Turns string of ...x1,y1\nx2,y2\n...
+        into row & column locations, which then populates a numpy array
+        to mark those positions
+        """
+        coords = np.array([x.split(",") for x in coords.split("\n")], dtype=int)
+        rows = coords[:, 1]
+        cols = coords[:, 0]
+        grid = np.zeros((rows.max() + 1, cols.max() + 1), dtype=int)
+        grid[rows, cols] = 1
+
+        return grid
+
+    def fold_vert(self, val) -> np.array:
+        """Offset is needed to deal w/ even vs odd number of rows"""
+        offset = int(self.grid.shape[0] % 2)
+
+        top = self.grid[:val, :]
+        bot = np.flipud(self.grid[val + offset :, :])
+        top[-val - offset :, :] = top[-val - offset :, :] | bot
+        return top
+
+    def fold_horiz(self, val) -> np.array:
+        """Offset is needed to deal w/ even vs odd number of columns"""
+        offset = int(self.grid.shape[1] % 2)
+
+        left = self.grid[:, :val]
+        right = self.grid[:, val + offset :]
+        right = np.fliplr(right)
+        left[:, -val - offset :] = left[:, -val - offset :] | right
+        return left
+
+    def fold(self) -> np.array:
+        """Read (& dispose of) the first 'fold' instruction and apply it to the grid."""
+        across_dim, val = self.folds.pop(0)
+
+        if across_dim == "x":
+            new_grid = self.fold_horiz(val)
+        if across_dim == "y":
+            new_grid = self.fold_vert(val)
+        self.grid = new_grid
+        return new_grid
 
     def part_1(self):
-        pass
+        self.fold()
+        return self.grid.sum()
+
+    def display(self, matrix=None):
+        if matrix is None:
+            matrix = self.grid
+        grid = np.where(matrix, "#", " ")
+
+        for row in grid:
+            print("".join(row))
+
+        return
 
     def part_2(self):
-        pass
+        while self.folds:
+            self.fold()
+
+        self.display()
+
+        return
 
 
 def run_tests(p1_ans=T1_ANS, p2_ans=T2_ANS, fname="tests.txt"):
@@ -115,9 +107,6 @@ def run_tests(p1_ans=T1_ANS, p2_ans=T2_ANS, fname="tests.txt"):
 
 if __name__ == "__main__":
     run_tests()
-    run_tests(19, None, fname='tests2.txt')
-    run_tests(226, None, fname='tests3.txt')
-
 
     puz = Puzzle("inputs.txt")
 
